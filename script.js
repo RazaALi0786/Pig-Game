1e0d6d27f8ce31953cbf39c907517c5b3d599fa0
Syed Raza Ali <razaali26296@gmail.com>
2023-12-14 09:49:32 +0530

    Third commit

--- script.js
+++ script.js
@@ -11,13 +11,23 @@ const btnNew = document.querySelector(".btn--new");
 const btnHold = document.querySelector(".btn--hold");
 const btnRoll = document.querySelector(".btn--roll");
 //Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-let currentScore = 0;
-let activePlayer = 0;
-const scores = [0, 0];
-diceEl.classList.add("hidden");
-let playing = true;
+let scores, playing, currentScore, activePlayer;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
 // Switch to next player
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -69,3 +79,4 @@ btnHold.addEventListener("click", function () {
   }
   // Finish
 });
+btnNew.addEventListener("click", init);
